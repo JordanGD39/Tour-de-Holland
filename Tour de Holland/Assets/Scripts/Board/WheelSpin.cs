d92eea741f92eacf4bbebd7b6aeee0db05c9505f
Ian Hartog <ianarthur33@gmail.com>
2022-03-01 14:47:30 +0100

    Scene en script aangemaakt

---
+++ Tour de Holland/Assets/Scripts/Board/WheelSpin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelSpin : MonoBehaviour
+{
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
