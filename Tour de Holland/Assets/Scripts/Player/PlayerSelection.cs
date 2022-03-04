2198d725bdeb22a516e47e7a9325cb6cc9c4db07
Everardus Tol <43673344+jobtolmop@users.noreply.github.com>
2022-03-04 13:49:08 +0100

    First implementation of CharacterSelection

---
+++ Tour de Holland/Assets/Scripts/Player/PlayerSelection.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelection : MonoBehaviour
+{
+    public enum CharacterSelection { Kaas, Klomp, Placeholder1, Placeholder2, Placeholder3, Placeholder4, Placeholder5, Placeholder6 };
+    [SerializeField] GameObject[] characterPrefabs;
+    
+}
