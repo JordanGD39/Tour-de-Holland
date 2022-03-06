ee5447b47cdea8432ed68655e2f043534d21588e
Ian Hartog <ianarthur33@gmail.com>
2022-03-06 23:45:32 +0100

    Wheel spin working with button press
    Wheel can be spun and stopped by pressing the button.

--- Tour de Holland/Assets/Scripts/Board/WheelSpin.cs
+++ Tour de Holland/Assets/Scripts/Board/WheelSpin.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class WheelSpin : MonoBehaviour
 {
     private Rigidbody rigid;
+    [SerializeField]
+    private float spinTime;
+    [SerializeField]
+    private bool isSpinning = false;
+    [SerializeField]
+    private float spinVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +21,22 @@ public class WheelSpin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Spin()
+    {
+        if (!isSpinning)
+        {
+            rigid.angularDrag = 0.05f;
+            spinVelocity = Random.Range(130, 200);
+            rigid.AddTorque(transform.up * spinVelocity);
+            isSpinning = true;
+        }
+       else if (isSpinning)
+        {
+            rigid.angularDrag = 20;
+            isSpinning = false;
+        }
     }
 }
