bb2aada8c47bc16d26441deaca830d69756e342f
Ian Hartog <ianarthur33@gmail.com>
2022-03-07 16:47:49 +0100

    Wheelspin Complete
    Wheelspin works and returns a number

--- Tour de Holland/Assets/Scripts/Board/WheelSpin.cs
+++ Tour de Holland/Assets/Scripts/Board/WheelSpin.cs
@@ -11,6 +11,15 @@ public class WheelSpin : MonoBehaviour
     private bool isSpinning = false;
     [SerializeField]
     private float spinVelocity;
+    private bool canSpin = true;
+
+    [SerializeField]
+    private float lerpTime = 3;
+    [SerializeField]
+    private float targetDrag = 20;
+
+    [SerializeField]
+    private Transform rayPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -18,25 +27,50 @@ public class WheelSpin : MonoBehaviour
         rigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void Spin()
     {
+        if (!canSpin)
+        {
+            return;
+        }
+
         if (!isSpinning)
         {
-            rigid.angularDrag = 0.05f;
+            rigid.angularDrag = 0f;
             spinVelocity = Random.Range(230, 300);
             rigid.AddTorque(transform.up * spinVelocity);
             isSpinning = true;
         }
        else if (isSpinning)
         {
-            rigid.angularDrag = 20;
+            canSpin = false;
+            StartCoroutine(LerpDrag());
             isSpinning = false;
+        }
+    }
+
+    private IEnumerator LerpDrag()
+    {
+        float startTime = Time.time;
+        float startDrag = rigid.angularDrag;
+
+        while (rigid.angularDrag < targetDrag)
+        {
+            float frac = (Time.time - startTime) / lerpTime;
+            rigid.angularDrag = Mathf.Lerp(startDrag, targetDrag, frac);
+            yield return null;
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayPoint.position, -Vector3.up, out hit, 100))
+        {
+            if (hit.collider.gameObject.CompareTag("Number"))
+            {
+                int number = hit.collider.transform.GetSiblingIndex() + 1;
+                Debug.Log(number);
+            }
         }
     }
 }
